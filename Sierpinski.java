b476971e108aa92cccea14e429a30f35fdc3d304
Ellie Elkon <ellieelkon@Ellies-MacBook-Pro.local>
2025-12-31 16:21:28 +0200

    done

--- Sierpinski.java
+++ Sierpinski.java
@@ -7,12 +7,53 @@ public class Sierpinski {
 	
 	// Draws a Sierpinski triangle of depth n on the standard canvass.
 	public static void sierpinski (int n) {
-		//// Replace this comment with your code
-	}
+		StdDraw.setScale(0.0, 1.0);
+        StdDraw.clear(StdDraw.WHITE);
+        StdDraw.setPenColor(StdDraw.BLACK);
+
+        double side = 0.9;
+        double height = side * Math.sqrt(3) / 2.0;
+
+        double xCenter = 0.5;
+        double yBottom = 0.05;
+
+        double x1 = xCenter - side / 2.0;
+        double y1 = yBottom;
+
+        double x2 = xCenter + side / 2.0;
+        double y2 = yBottom;
+
+        double x3 = xCenter;
+        double y3 = yBottom + height;
+
+        StdDraw.line(x1, y1, x2, y2);
+        StdDraw.line(x2, y2, x3, y3);
+        StdDraw.line(x3, y3, x1, y1);
+
+        sierpinski(n, x1, x2, x3, y1, y2, y3);
+    }
 	
 	// Does the actual drawing, recursively.
 	private static void sierpinski(int n, double x1, double x2, double x3,
 		                                 double y1, double y2, double y3) {
-		//// Replace this comment with your code
+ 		if (n > 0)
+		{
+        double x12 = (x1 + x2) / 2.0;
+        double y12 = (y1 + y2) / 2.0;
+
+        double x23 = (x2 + x3) / 2.0;
+        double y23 = (y2 + y3) / 2.0;
+
+        double x31 = (x3 + x1) / 2.0;
+        double y31 = (y3 + y1) / 2.0;
+
+        StdDraw.line(x12, y12, x23, y23);
+        StdDraw.line(x23, y23, x31, y31);
+        StdDraw.line(x31, y31, x12, y12);
+
+        sierpinski(n - 1, x1,  x12, x31, y1,  y12, y31);
+        sierpinski(n - 1, x12, x2,  x23, y12, y2,  y23);
+        sierpinski(n - 1, x31, x23, x3,  y31, y23, y3);
+		}
 	}
 }
